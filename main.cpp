5d0a6e2a0d32d9af679309a163b69701b1b83514
jeffersonnasciment0 <jeffersonsantos.99@hotmail.com>
2026-01-16 16:08:28 -0300

    testing modify user git

--- main.cpp
+++ main.cpp
@@ -9,12 +9,13 @@ int main(){
     cout << "Teste\n";
 
     Device disp1("tv");
-
     Device disp2("radio");
+    Device disp3("mouse");
 
     devicesManager manager;
     manager.addDevice(disp1);
     manager.addDevice(disp2);
+    manager.addDevice(disp3);
 
     return 0;
 }
